68bfd7b0ed14ef7f6a825db23023c6db6df190ac
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2020-12-28 14:20:42 -0500

    added a huge mess of pseudo code outlining the plan

--- js/main.js
+++ js/main.js
@@ -1,3 +1,79 @@
 const datePicker = document.getElementById('datePicker');
+ 
+const test = document.getElementById('test');
 
-console.log(datePicker)
+test.addEventListener('click', () => {
+let splitDates = datePicker.value.split('-');
+console.log(splitDates);
+let month = Number(splitDates[1])
+let dayDate = Number(splitDates[2])
+console.log(month, dayDate)
+});
+1992-01-28
+
+01 28
+01 + 28
+0128
+
+
+Aquarius (January: 1 20 - February: 2 18) 120 - 218
+
+if(date >= 120 && <= 218) then Wolfie is an Aquarius so now do stuff
+
+Range(start, stop) {
+    if(start )
+}
+
+Range(splitDates[1], splitDates[2]);
+
+
+const starSign = {
+
+
+{Taurus:[ "value1","value2","value"]}
+
+Gemini (May 21 - June 20)
+
+Cancer (June 21 - July 22)
+
+Leo (July 23 - August 22)
+
+Virgo (August 23 - September 22)
+
+Libra (September 23 - October 22)
+
+Scorpio (October 23 - November 21)
+
+Sagittarius (November 22 - December 21)
+
+Capricorn (December 22 - January 19)
+
+Aquarius (January 20 - February 18)
+
+Pisces (February 19 - March 20)
+    
+}
+
+// Aries (March 21 - April 19)
+
+// Taurus (April 20 - May 20)
+
+// Gemini (May 21 - June 20)
+
+// Cancer (June 21 - July 22)
+
+// Leo (July 23 - August 22)
+
+// Virgo (August 23 - September 22)
+
+// Libra (September 23 - October 22)
+
+// Scorpio (October 23 - November 21)
+
+// Sagittarius (November 22 - December 21)
+
+// Capricorn (December 22 - January 19)
+
+// Aquarius (January 20 - February 18)
+
+// Pisces (February 19 - March 20)
