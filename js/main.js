79bc98abc983f8a027f58bc5c979bb247b13ece2
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2020-12-28 18:43:59 -0500

    deleted the messy pseudocode, fixed the comparison function, added upside down conditionals, changed them to right-side up, Henrik made a button and then he left and then David and I made it work better.

--- js/main.js
+++ js/main.js
@@ -2,78 +2,64 @@ const datePicker = document.getElementById('datePicker');
  
 const test = document.getElementById('test');
 
-test.addEventListener('click', () => {
-let splitDates = datePicker.value.split('-');
-console.log(splitDates);
-let month = Number(splitDates[1])
-let dayDate = Number(splitDates[2])
-console.log(month, dayDate)
-});
-1992-01-28
+const placeToShowYourFuture = document.getElementById('placeToShowYourFuture');
 
-01 28
-01 + 28
-0128
+const yourStarSign = document.getElementById('yourStarSign');
+
+const yourFuturePrediciton = document.getElementById('yourFuturePrediciton');
+
+const button = document.getElementById('button');
 
 
-Aquarius (January: 1 20 - February: 2 18) 120 - 218
 
-if(date >= 120 && <= 218) then Wolfie is an Aquarius so now do stuff
-
-Range(start, stop) {
-    if(start )
-}
-
-Range(splitDates[1], splitDates[2]);
-
-
-const starSign = {
-
-
-{Taurus:[ "value1","value2","value"]}
-
-Gemini (May 21 - June 20)
-
-Cancer (June 21 - July 22)
-
-Leo (July 23 - August 22)
-
-Virgo (August 23 - September 22)
-
-Libra (September 23 - October 22)
-
-Scorpio (October 23 - November 21)
-
-Sagittarius (November 22 - December 21)
-
-Capricorn (December 22 - January 19)
-
-Aquarius (January 20 - February 18)
-
-Pisces (February 19 - March 20)
+button.addEventListener('click', () => {
+    let splitDates = datePicker.value.split('-');
+    console.log(splitDates);
+    let dateToCompare = Number(splitDates[1] + splitDates[2])
     
-}
+    if(dateToCompare > 1221){
+        yourStarSign.innerText = 'Capricorn'  
+    }else if(dateToCompare > 1121){
+        yourStarSign.innerText = 'Sagittarius'
+    }else if(dateToCompare > 1022){
+        yourStarSign.innerText = 'Scorpio'
+    }else if(dateToCompare > 922){
+        yourStarSign.innerText = 'Libra'
+    }else if(dateToCompare > 822){
+        yourStarSign.innerText = 'Virgo'
+    }else if(dateToCompare > 722){
+        yourStarSign.inenrText = 'Leo'
+    }else if(dateToCompare > 620){
+        yourStarSign.innerText = 'Cancer'
+    }else if(dateToCompare > 520){
+        yourStarSign.innerText = 'Geminis'
+    }else if(dateToCompare > 419){
+        yourStarSign.innerText = 'Taurus'
+    }else if(dateToCompare > 320){
+        yourStarSign.innerText = 'Aries'
+    }else if(dateToCompare > 218){
+        yourStarSign.innerText = 'Pisces'
+    }else if(dateToCompare > 119){
+        yourStarSign.innerText = 'Aquarius'
+    }else if(dateToCompare > 100 ){
+        yourStarSign.innerText = 'Capricorn'
+        console.log(dateToCompare);
+    }
+    
+    let bullshit = `Oh wow, you are a ${yourStarSign.innerText}! How wonderful, all people with that star sign are succsessful and rich!`;
+    yourFuturePrediciton.innerText = bullshit;
+    datePicker.classList.toggle('hidden');
+    button.classList.toggle('hidden');
+    placeToShowYourFuture.classList.toggle('hidden');
+    yourFuturePrediciton.classList.toggle('hidden');
+    
+});
 
-// Aries (March 21 - April 19)
 
-// Taurus (April 20 - May 20)
+button.addEventListener("mouseover", () =>{
+    button.classList.replace('button', 'hover')
 
-// Gemini (May 21 - June 20)
-
-// Cancer (June 21 - July 22)
-
-// Leo (July 23 - August 22)
-
-// Virgo (August 23 - September 22)
-
-// Libra (September 23 - October 22)
-
-// Scorpio (October 23 - November 21)
-
-// Sagittarius (November 22 - December 21)
-
-// Capricorn (December 22 - January 19)
-
-// Aquarius (January 20 - February 18)
-
-// Pisces (February 19 - March 20)
+    button.onmouseout = () => {
+    button.classList.replace('hover', 'button');
+    }
+})
